fb6d3b53a96658629d51c6d1f114dc02c573abf6
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-12-19 18:00:15 +0900

    feat: init project files

---
+++ come-on-toeic/src/main/java/com/comeontoeic/ComeOnToeicApplication.java
@@ -0,0 +1,13 @@
+package com.comeontoeic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeOnToeicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeOnToeicApplication.class, args);
+	}
+
+}
---
+++ come-on-toeic/src/test/java/com/comeontoeic/ComeOnToeicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeontoeic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeOnToeicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

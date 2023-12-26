0af849632c11350fcac10a690bcc3ad5d2ad5f17
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2023-12-26 17:04:54 +0900

    feat: appspec.yml, deploy.sh 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/come-on-toeic
+cd $REPOSITORY
+
+APP_NAME=come-on-toeic
+JAR_NAME=$(ls $REPOSITORY/build/libs/ \ grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NANME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &

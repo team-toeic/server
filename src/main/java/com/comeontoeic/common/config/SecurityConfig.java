8305ba8bc26d8acd6c9b76bb21bcdbf6542a6adf
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2024-01-09 17:00:25 +0900

    refactor: cors Error resolve

--- src/main/java/com/comeontoeic/common/config/SecurityConfig.java
+++ src/main/java/com/comeontoeic/common/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -35,10 +36,24 @@ public class SecurityConfig {
                         .requestMatchers("/endpoint").hasAuthority("ROLE_USER")
                         .requestMatchers("/register", "/login").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/users/**").hasRole("USER")
                         .anyRequest().denyAll())
                 ;
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }

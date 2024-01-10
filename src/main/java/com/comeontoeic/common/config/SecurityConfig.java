6a5d3e3004bc4354242f3638cafb9b688895df6b
Min Kevin <58409670+MinKevin@users.noreply.github.com>
2024-01-10 18:24:57 +0900

    refactor: fix ci/cd error

--- src/main/java/com/comeontoeic/common/config/SecurityConfig.java
+++ src/main/java/com/comeontoeic/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.comeontoeic.common.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
@@ -40,7 +41,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/endpoint").hasAuthority("ROLE_USER")
-                        .requestMatchers("/register", "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/login", "/users/signup").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/users/**").hasRole("USER")
                         .anyRequest().denyAll())
